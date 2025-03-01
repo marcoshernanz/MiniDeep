ad2108c3486063fce28823235d2a72197d52ced8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-03-01 10:32:50 +0100

    Remove unused theme utility files and simplify ThemeProvider usage in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
+import ThemeProvider from "@/components/ThemeProvider";
 import "@/global.css";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? "dark" : "light"}>
+    <ThemeProvider>
       <SafeAreaProvider>
         <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
         <SafeAreaView className="flex-1 bg-background">
