e869d34884bcf98408b5970fe0243fe6406c0c5e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-03-02 12:38:15 +0100

    Refactor time selection in IndexScreen to use TimePicker component; remove old TimeSelector and TimePicker implementations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { TimeSelector } from "@/components/TimeSelector";
-import WheelNumberPicker from "@/components/ui/WheelNumberPicker";
 import { Button } from "@/components/ui/button";
+import TimePicker from "@/components/deep-work/TimePicker";
 
 export default function IndexScreen() {
   const [hours, setHours] = useState(0);
@@ -13,27 +12,12 @@ export default function IndexScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
       <View className="gap-10">
-        <View className="relative">
-          <View className="flex-row items-center justify-center">
-            <WheelNumberPicker
-              number={hours}
-              setNumber={setHours}
-              minValue={0}
-              maxValue={23}
-              containerHeight={250}
-            />
-            <Text className="h-full align-middle text-6xl font-medium">:</Text>
-            <WheelNumberPicker
-              number={minutes}
-              setNumber={setMinutes}
-              minValue={0}
-              maxValue={59}
-              interval={5}
-              containerHeight={250}
-            />
-          </View>
-          <View className="pointer-events-none absolute left-0 right-0 top-1/2 -mx-2 h-[4.5rem] -translate-y-1/2 rounded-lg border-2 border-primary"></View>
-        </View>
+        <TimePicker
+          hours={hours}
+          setHours={setHours}
+          minutes={minutes}
+          setMinutes={setMinutes}
+        />
         <View>
           <Button size="lg" className="w-full">
             <Text className="native:text-2xl">Start</Text>
