cad54200b3282cb59136a976911fb0588ee8f513
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-03-02 12:48:24 +0100

    Implement timer functionality in IndexScreen with start button and formatted display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,25 +4,51 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "@/components/ui/button";
 import TimePicker from "@/components/deep-work/TimePicker";
+import padWithZeros from "@/lib/padWithZeros";
 
 export default function IndexScreen() {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(30);
 
+  const [hasStarted, setHasStarted] = useState(false);
+
+  const handleStartTimer = () => {
+    setHasStarted(true);
+  };
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
       <View className="gap-10">
-        <TimePicker
-          hours={hours}
-          setHours={setHours}
-          minutes={minutes}
-          setMinutes={setMinutes}
-        />
-        <View>
-          <Button size="lg" className="w-full">
-            <Text className="native:text-2xl">Start</Text>
-          </Button>
-        </View>
+        {!hasStarted && (
+          <>
+            <TimePicker
+              hours={hours}
+              setHours={setHours}
+              minutes={minutes}
+              setMinutes={setMinutes}
+            />
+            <View>
+              <Button size="lg" className="w-full" onPress={handleStartTimer}>
+                <Text className="native:text-2xl">Start</Text>
+              </Button>
+            </View>
+          </>
+        )}
+
+        {hasStarted && (
+          <>
+            <View className="size-64 flex-row items-center justify-center rounded-full border-2 border-primary">
+              <Text className="w-20 text-center text-6xl">
+                {padWithZeros(hours, 2)}
+              </Text>
+              <Text className="text-6xl font-medium">:</Text>
+              <Text className="w-20 text-center text-6xl">
+                {padWithZeros(minutes, 2)}
+              </Text>
+            </View>
+            <View></View>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
