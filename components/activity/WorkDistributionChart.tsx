825d2e8cebc3a3d1a487d90137c0665578150bbc
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-03-18 21:13:59 +0100

    tooltip for WorkDistributionChart

--- components/activity/WorkDistributionChart.tsx
+++ components/activity/WorkDistributionChart.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Platform, View } from "react-native";
+import { View } from "react-native";
 import { Text } from "../ui/text";
 import { ActivityType } from "@/lib/hooks/useActivity";
 import { Card } from "../ui/card";
-import { CartesianChart, Area, Line } from "victory-native";
+import { CartesianChart, Area, Line, useChartPressState } from "victory-native";
 import {
-  FontStyle,
+  Circle,
   LinearGradient,
   listFontFamilies,
   matchFont,
-  Skia,
-  useFont,
 } from "@shopify/react-native-skia";
 import useColors from "@/lib/hooks/useColors";
-import { SkFont } from "@shopify/react-native-skia";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
 
 interface Props {
   sessions: ActivityType["sessions"];
@@ -46,33 +44,49 @@ const DATA = [
   { duration: 0, hour: 23 },
 ];
 
+const circleSize = 12;
+
 export default function WorkDistributionChart({ sessions }: Props) {
   const { getColor } = useColors();
   const font = matchFont({ fontFamily: listFontFamilies()[0] });
+  const { state, isActive } = useChartPressState({ x: 0, y: { duration: 0 } });
 
-  console.log(font);
+  const lineStyle = useAnimatedStyle(() => ({
+    width: 1,
+    transform: [{ translateX: state.x.position.value - 0.5 }],
+  }));
+  const circleStyle = useAnimatedStyle(() => ({
+    height: circleSize,
+    width: circleSize,
+    transform: [
+      { translateX: state.x.position.value - circleSize / 2 },
+      { translateY: state.y.duration.position.value - circleSize / 2 },
+    ],
+  }));
 
   return (
     <Card className="mx-4 p-4">
-      <Text className="mb-2 text-lg font-medium text-foreground">
+      <Text className="mb-2 text-xl font-medium text-foreground">
         Daily Work Distribution
       </Text>
-      <View className="h-64">
+      <View className="h-72">
         <CartesianChart
           data={DATA}
           xKey="hour"
           yKeys={["duration"]}
           domain={{ x: [0, 23], y: [-1, 61] }}
-          padding={{ bottom: 10, top: 10 }}
+          padding={{ bottom: 12, top: 40 }}
           xAxis={{
             font,
             lineWidth: 0,
             tickValues: [4, 9, 14, 19],
+            labelOffset: 5,
             lineColor: getColor("mutedForeground"),
             labelColor: getColor("mutedForeground"),
             formatXLabel: (x) => (x < 10 ? `0${x}:00` : `${x}:00`),
           }}
           yAxis={[{ lineWidth: 0 }]}
+          chartPressState={state}
         >
           {({ points, chartBounds }) => (
             <>
@@ -97,6 +111,21 @@ export default function WorkDistributionChart({ sessions }: Props) {
             </>
           )}
         </CartesianChart>
+
+        {isActive && (
+          <>
+            <View className="absolute h-full">
+              <Animated.View
+                className="mb-6 mt-9 flex-1 bg-primary"
+                style={lineStyle}
+              ></Animated.View>
+            </View>
+            <Animated.View
+              className="absolute rounded-full bg-primary"
+              style={circleStyle}
+            ></Animated.View>
+          </>
+        )}
       </View>
     </Card>
   );
