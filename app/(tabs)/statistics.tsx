cd894879f64e8f9a5bfd050349149dd7b158f8ee
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-03-22 09:59:42 +0100

    statistics header

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,1 +1,10 @@
-export default function StatsScreen() {}
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function StatsScreen() {
+  return (
+    <GestureHandlerRootView className="flex-1">
+      <SafeAreaView className="flex-1 bg-background py-6"></SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
