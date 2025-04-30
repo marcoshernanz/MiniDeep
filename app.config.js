e75fdc18f8a70f85d21d703a6df51c641f72399a
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-04-30 10:22:11 +0200

    configure eas

---
+++ app.config.js
@@ -0,0 +1,36 @@
+const isDevelopment = process.env.APP_VARIANT === "development";
+const isPreview = process.env.APP_VARIANT === "preview";
+
+const getUniqueIdentifier = () => {
+  if (isDevelopment) {
+    return "com.marcoshernanz.minideep.dev";
+  } else if (isPreview) {
+    return "com.marcoshernanz.minideep.preview";
+  } else {
+    return "com.marcoshernanz.minideep";
+  }
+};
+
+const getAppName = () => {
+  if (isDevelopment) {
+    return "MiniDeep (Dev)";
+  } else if (isPreview) {
+    return "MiniDeep (Preview)";
+  } else {
+    // return "MiniDeep: A Minimalistic Deep Work Tracker";
+    return "MiniDeep (Prod)";
+  }
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
+});
