ca3e3fe0c898e58e6a2dc95ab13a9be5c0019500
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-05-14 10:09:10 +0200

    stopwatch context

--- context/StopwatchContext.tsx
+++ context/StopwatchContext.tsx
@@ -1,20 +1,25 @@
-import useTimer from "@/lib/hooks/useTimer";
-import { TrackerState } from "@/zod/schemas/TrackerStateSchema";
-import { createContext, useContext, useState } from "react";
 import useStopwatch from "@/lib/hooks/useStopwatch";
+import { TrackerState } from "@/zod/schemas/TrackerStateSchema";
+import { createContext, useContext } from "react";
 
 interface StopwatchContextValue {
-  status: TrackerState["status"];
-  startStopwatch: () => void;
-  togglePause: () => void;
-  stopStopwatch: () => void;
+  stopwatch: {
+    timeElapsed: number;
+    status: TrackerState["status"];
+    startStopwatch: () => void;
+    togglePause: () => void;
+    stopStopwatch: () => void;
+  };
 }
 
 export const StopwatchContext = createContext<StopwatchContextValue>({
-  status: "inactive",
-  startStopwatch: () => {},
-  togglePause: () => {},
-  stopStopwatch: () => {},
+  stopwatch: {
+    timeElapsed: 0,
+    status: "inactive",
+    startStopwatch: () => {},
+    togglePause: () => {},
+    stopStopwatch: () => {},
+  },
 });
 
 interface Props {
