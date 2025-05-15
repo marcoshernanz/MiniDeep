adcd33a68835fd483acdfd827a51f3a064154adb
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-05-15 19:18:51 +0200

    fix duplicate safe area view

--- components/tracker/Tracker.tsx
+++ components/tracker/Tracker.tsx
@@ -1,5 +1,4 @@
 import { useTrackerContext } from "@/context/TrackerContext";
-import { SafeAreaView } from "react-native-safe-area-context";
 import TrackerPickerScreen from "./TrackerPickerScreen";
 import Timer from "./timer/Timer";
 import Stopwatch from "./stopwatch/Stopwatch";
@@ -8,12 +7,12 @@ export default function Tracker() {
   const { trackerType } = useTrackerContext();
 
   return (
-    <SafeAreaView className="flex-1">
+    <>
       {trackerType === null && <TrackerPickerScreen />}
 
       {trackerType === "timer" && <Timer />}
 
       {trackerType === "stopwatch" && <Stopwatch />}
-    </SafeAreaView>
+    </>
   );
 }
