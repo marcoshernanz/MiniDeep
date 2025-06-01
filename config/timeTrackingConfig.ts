5690f447e7152594d29ad60bc4f5a38520a9904f
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-01 14:26:48 +0200

    different data for production and development

--- config/timeTrackingConfig.ts
+++ config/timeTrackingConfig.ts
@@ -1,5 +1,18 @@
-const timeTrackingConfig = {
-  storageKey: "minideep_work_sessions",
-} as const;
+import Constants from "expo-constants";
+
+const appVariant = Constants.expoConfig?.extra?.APP_VARIANT as
+  | string
+  | undefined;
+const isDevelopment = appVariant === "development";
+const isPreview = appVariant === "preview";
+
+const baseKey = "minideep_work_sessions";
+const storageKey = isDevelopment
+  ? `${baseKey}_dev`
+  : isPreview
+    ? `${baseKey}_preview`
+    : baseKey;
+
+const timeTrackingConfig = { storageKey };
 
 export default timeTrackingConfig;
--- config/trackerStateConfig.ts
+++ config/trackerStateConfig.ts
@@ -1,3 +1,16 @@
-export const trackerStateConfig = {
-  storageKey: "minideep_tracker_state",
-} as const;
+import Constants from "expo-constants";
+
+const appVariant = Constants.expoConfig?.extra?.APP_VARIANT as
+  | string
+  | undefined;
+const isDevelopment = appVariant === "development";
+const isPreview = appVariant === "preview";
+
+const baseKey = "minideep_tracker_state";
+const storageKey = isDevelopment
+  ? `${baseKey}_dev`
+  : isPreview
+    ? `${baseKey}_preview`
+    : baseKey;
+
+export const trackerStateConfig = { storageKey };
