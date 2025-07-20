b966281c0e978cb5ad7f0900a5f813944ef254b5
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-07-20 13:52:22 +0200

    migrate babel config

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: ["react-native-reanimated/plugin"],
-    env: {
-      production: {
-        plugins: ["transform-remove-console"],
-      },
-    },
-  };
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: ["react-native-reanimated/plugin"],
 };
