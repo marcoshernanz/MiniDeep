e281f45f56a3c58a42ae0ba8de88a1b4f68abd6c
MarcosH911 <marcos.hernanz.anton@gmail.com>
2025-07-29 17:13:35 +0200

    activity

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -1,12 +1,47 @@
-import Activity from "@/components/activity/Activity";
-import ActivityContextProvider from "@/context/ActivityContext";
+import SafeArea from "@/components/ui/SafeArea";
+import Title from "@/components/ui/Title";
+import { Dimensions, FlatList, StyleSheet } from "react-native";
+import useActivity from "@/lib/hooks/useActivity";
 
 export default function ActivityScreen() {
-  return null;
+  const data = useActivity();
 
-  // return (
-  //   <ActivityContextProvider>
-  //     <Activity />
-  //   </ActivityContextProvider>
-  // );
+  const screenWidth = Dimensions.get("window").width;
+
+  return (
+    <SafeArea style={styles.safeArea} edges={["top"]}>
+      <Title style={styles.title}>Activity</Title>
+      <FlatList
+        data={data}
+        style={{ flex: 1 }}
+        horizontal
+        pagingEnabled
+        directionalLockEnabled
+        showsHorizontalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+        keyExtractor={({ date }) => date.toDateString()}
+        initialScrollIndex={data.length - 1}
+        getItemLayout={(_, index) => ({
+          length: screenWidth,
+          offset: screenWidth * index,
+          index,
+        })}
+        renderItem={({ item }) => (
+          <ActivityItem key={item.date.toDateString()} item={item} />
+        )}
+        overScrollMode="never"
+        bounces={false}
+        alwaysBounceVertical={false}
+      />
+    </SafeArea>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    paddingHorizontal: 0,
+  },
+  title: {
+    paddingHorizontal: 16,
+  },
+});
