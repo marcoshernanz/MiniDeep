5ff053db89c12a75dda706c32f24d42d864d9e3f
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-08-19 20:34:21 +0200

    fix notification events handling

--- index.ts
+++ index.ts
@@ -2,8 +2,25 @@ import "react-native-get-random-values";
 
 import "expo-router/entry";
 
-import notifee from "@notifee/react-native";
+import notifee, { Event, EventType } from "@notifee/react-native";
+import { getStopwatchControls } from "./lib/controls/stopwatchControls";
+
+async function handleNotifeeEvents(event: Event) {
+  if (event.type === EventType.ACTION_PRESS) {
+    const pressActionId = event.detail.pressAction?.id;
+    if (pressActionId === "pause") {
+      getStopwatchControls()?.togglePause();
+    } else if (pressActionId === "resume") {
+      getStopwatchControls()?.togglePause();
+    } else if (pressActionId === "stop") {
+      getStopwatchControls()?.stop();
+    }
+  }
+}
 
 notifee.registerForegroundService((notification) => {
   return new Promise(() => {});
 });
+
+notifee.onForegroundEvent(handleNotifeeEvents);
+notifee.onBackgroundEvent(handleNotifeeEvents);
