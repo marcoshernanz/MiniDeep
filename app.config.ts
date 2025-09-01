b013bc24863e95b729ae8ecf2a59556851ba0f5c
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-01 21:14:03 +0200

    bump up version

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "MiniDeep",
-  version: "1.1.0",
+  version: "1.1.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "minideep",
